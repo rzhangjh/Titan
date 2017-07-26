5f4c08aaeeeb2256d821f3420efa98918e279810
rzhangjh <r.zhang.jh@gmail.com>
2017-07-26 11:08:35 +0800

    First commit.

---
+++ src/rpc/Test.java
@@ -0,0 +1,45 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html><html><head><title>Animals Around the World</title><h1>The BROWN BEAR</h1><p>The brown bear (Ursus arctos) is native to parts of northern Eurasia and North America. Its conservation status is currently \"Least Concern.\" There are many subspecies within the brown bear species, including the Atlas bear and the Himalayan brown bear.</p><a href = \"https://en.wikipedia.org/wiki/Brown_bear\" target = \"_blank\">Learn More</a></head></html>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
